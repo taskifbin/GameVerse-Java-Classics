37be500cc524c20bda393355744076eafa1d0c1c
taskifbin <taskifbin@gmail.com>
2025-06-01 08:55:08 +0600

    Added Interface for gameboard and Controller class reusebilty

---
+++ ClassicalSnake/src/GameConstants.java
@@ -0,0 +1,7 @@
+public interface GameConstants {
+    int ScreeenWidth = 800;
+    int ScreenHeight = 600;
+    int unit = 20;
+    int gameUnit = (ScreenHeight * unit) / (unit * unit);
+    int Delay = 80;
+}
