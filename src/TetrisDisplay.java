7d9d1db2b15f4e3c9e989a2087255a017e797b49
Tanvir Alam Shihab <shihabvs02@gmail.com>
2025-06-02 20:06:47 +0600

    Delete MyFrame and add Tetris Panel

--- src/TetrisDisplay.java
+++ src/TetrisDisplay.java
@@ -1,27 +1,23 @@
 import javax.swing.*;
 import java.awt.*;
 
-public class TetrisDisplay extends JFrame {
-    MyFrame frame = new MyFrame();
-    JPanel GamePanel = new JPanel();
-    JPanel ScorePanel = new JPanel();
-    JPanel MenuPanel = new JPanel();
-    JPanel ButtonPanel = new JPanel();
+public class TetrisDisplay {
+    TetrisPanel gamePanel = new TetrisPanel();
+    JFrame TetrisFrame = new JFrame();
+
     public TetrisDisplay() {
-        GamePanel.setBackground(Color.BLACK);
-        GamePanel.setBounds(250,80,500,750);
-        GamePanel.setBorder(BorderFactory.createLineBorder(Color.DARK_GRAY));
 
-        ScorePanel.setBackground(Color.LIGHT_GRAY);
-        ScorePanel.setBounds(750,80,250,750);
-
-        MenuPanel.setBackground(Color.LIGHT_GRAY);
-        MenuPanel.setBounds(0,80,250,750);
+        // Tetris Frame
+        TetrisFrame.setTitle("Tetris");
+        TetrisFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        TetrisFrame.setResizable(false);
+        TetrisFrame.setLocationRelativeTo(null);
+        TetrisFrame.setVisible(true);
 
 
+        // adding item to Tetris Frame
+        TetrisFrame.add(gamePanel);
+        TetrisFrame.pack();
 
-        frame.add(GamePanel);
-        frame.add(ScorePanel);
-        frame.add(MenuPanel);
     }
 }
---
+++ src/TetrisPanel.java
@@ -0,0 +1,62 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class TetrisPanel extends JPanel implements Runnable {
+    public static final int WIDTH = 1400;
+    public static final int HEIGHT = 1000;
+    final int FPS = 60;
+
+    Thread TetrisThread;
+    TetrisManager tetrisManager;
+
+    public TetrisPanel() {
+
+        // panel
+        this.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        this.setBackground(Color.BLACK);
+        this.setLayout(null);
+
+        // Tetris Manager
+        tetrisManager = new TetrisManager();
+
+    }
+    public void LaunchGame(){
+        TetrisThread = new Thread(this);
+        TetrisThread.start(); // it automatic called run method
+    }
+
+    @Override
+    public void run() {
+
+        // Game Loop ---> 1. Update() 2.repaint() 60 times per second
+        double drawInterval = 1000000000.0/FPS;
+        double delta = 0;
+        long lastTime = System.nanoTime();
+        long currentTime;
+
+        while(TetrisThread !=null){
+
+            currentTime = System.nanoTime();
+
+            delta += (currentTime - lastTime) / drawInterval;
+            lastTime = currentTime;
+
+            if(delta >= 1){
+                update();
+                repaint();
+                delta--;
+            }
+        }
+    }
+
+    private void update(){
+        tetrisManager.update();
+    }
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+
+        Graphics2D g2 = (Graphics2D) g;
+
+        tetrisManager.draw(g2);
+    }
+}
